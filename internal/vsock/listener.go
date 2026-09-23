cb72125dca57ad5f171ac28b1c3b6aa30d337552
Yibo Zhuang <yzhuang@openai.com>
2026-09-23 09:12:53 -0700

    Mark VSOCK sockets close-on-exec (#69)

--- internal/vsock/listener.go
+++ internal/vsock/listener.go
@@ -2,9 +2,11 @@ package vsock
 
 import (
 	"fmt"
-	"golang.org/x/sys/unix"
 	"net"
 	"os"
+	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 type listener struct {
@@ -13,58 +15,50 @@ type listener struct {
 }
 
 func Listen(port uint32) (net.Listener, error) {
-	fd, err := unix.Socket(unix.AF_VSOCK, unix.SOCK_STREAM, 0)
+	socketFD, err := newSocket(unix.AF_VSOCK)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := unix.SetNonblock(fd, true); err != nil {
-		return nil, err
-	}
-
-	file := os.NewFile(uintptr(fd), "vsock")
-
-	if err := unix.Bind(int(file.Fd()), &unix.SockaddrVM{
+	if err := unix.Bind(socketFD, &unix.SockaddrVM{
 		CID:  unix.VMADDR_CID_ANY,
 		Port: port,
 	}); err != nil {
+		_ = unix.Close(socketFD)
 		return nil, err
 	}
 
-	if err := unix.Listen(int(file.Fd()), unix.SOMAXCONN); err != nil {
+	if err := unix.Listen(socketFD, unix.SOMAXCONN); err != nil {
+		_ = unix.Close(socketFD)
 		return nil, err
 	}
 
 	return &listener{
-		file: file,
+		file: os.NewFile(uintptr(socketFD), "vsock"),
 		port: port,
 	}, nil
 }
 
 func (listener *listener) Accept() (net.Conn, error) {
-	fd, _, err := unix.Accept(int(listener.file.Fd()))
+	socketFD, err := acceptSocket(listener.file)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := unix.SetNonblock(fd, true); err != nil {
-		return nil, err
-	}
-
-	file := os.NewFile(uintptr(fd), "vsock")
-
-	peerName, err := unix.Getpeername(int(file.Fd()))
+	peerName, err := unix.Getpeername(socketFD)
 	if err != nil {
+		_ = unix.Close(socketFD)
 		return nil, fmt.Errorf("failed to get a peer name for an AF_VSOCK connection %w", err)
 	}
 
 	peerNameVM, ok := peerName.(*unix.SockaddrVM)
 	if !ok {
+		_ = unix.Close(socketFD)
 		return nil, fmt.Errorf("accepted a non-AF_VSOCK connection on an AF_VSOCK socket")
 	}
 
 	return &conn{
-		file:       file,
+		file:       os.NewFile(uintptr(socketFD), "vsock"),
 		localPort:  listener.port,
 		remotePort: peerNameVM.Port,
 	}, nil
@@ -76,4 +70,52 @@ func (listener *listener) Addr() net.Addr {
 
 func (listener *listener) Close() error {
 	return listener.file.Close()
+}
+
+func newSocket(domain int) (int, error) {
+	syscall.ForkLock.RLock()
+	defer syscall.ForkLock.RUnlock()
+
+	socketFD, err := unix.Socket(domain, unix.SOCK_STREAM, 0)
+	if err != nil {
+		return -1, err
+	}
+	unix.CloseOnExec(socketFD)
+
+	if err := unix.SetNonblock(socketFD, true); err != nil {
+		_ = unix.Close(socketFD)
+		return -1, err
+	}
+	return socketFD, nil
+}
+
+func acceptSocket(file *os.File) (int, error) {
+	raw, err := file.SyscallConn()
+	if err != nil {
+		return -1, err
+	}
+
+	var socketFD int
+	var acceptErr error
+	err = raw.Control(func(listenerFD uintptr) {
+		syscall.ForkLock.RLock()
+		defer syscall.ForkLock.RUnlock()
+
+		socketFD, _, acceptErr = unix.Accept(int(listenerFD))
+		if acceptErr != nil {
+			return
+		}
+		unix.CloseOnExec(socketFD)
+	})
+	if err != nil {
+		return -1, err
+	}
+	if acceptErr != nil {
+		return -1, acceptErr
+	}
+	if err := unix.SetNonblock(socketFD, true); err != nil {
+		_ = unix.Close(socketFD)
+		return -1, err
+	}
+	return socketFD, nil
 }
